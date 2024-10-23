ad4538af8147c6ef0ccb3d3c1a7298deb18355ff
Ronit2102 <2023.ronit.sahoo@ves.ac.in>
2024-10-23 22:12:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main{public static void main(String[] args){System.out.println("hello there");}}
+public class Main{public static void main(String[] args){System.out.println("hello world");}}
